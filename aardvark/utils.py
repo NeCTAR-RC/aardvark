89e99fbd0e9b31b0bf2f0b253bc36891f4d8185e
Sam Morrison <sorrison@gmail.com>
2022-09-30 11:20:56 +1000

    Switch to collections.abc for Iterable, removed in py3.10
    Change-Id: I6053cc02e1f0760309030f3a4e19f4ddd0cf31ca

--- aardvark/utils.py
+++ aardvark/utils.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from collections import Iterable
+from collections.abc import Iterable
 from datetime import datetime
 import eventlet.queue
 import eventlet.timeout
